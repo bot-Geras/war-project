eb175c954935e8f410fe437a7302d5ff804fc0a4
Brian <briandave771@gmail.com>
2022-06-04 02:22:33 +0300

    display the drawn cards

--- index.js
+++ index.js
@@ -1,58 +1,23 @@
+let deckId;
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      console.log(data);
+      deckId = data.deck_id;
+    });
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick);
-
-// function callback() {
-//     console.log("I finally ran!")
-// }
-
-// setTimeout(callback, 2000)
-
-// const people = [
-//     { name: "Jack", hasPet: true },
-//     { name: "Jill", hasPet: false },
-//     { name: "Alice", hasPet: true },
-//     { name: "Bob", hasPet: false },
-// ]
-
-// function gimmeThePets(number) {
-//     return person.hasPet
-// }
-
-// const peopleWithPets = people.filter(gimmeThePets)
-// console.log(peopleWithPets)
-
-const people = [
-  { name: "Jack", hasPet: true },
-  { name: "Jill", hasPet: false },
-  { name: "Alice", hasPet: true },
-  { name: "Bob", hasPet: false },
-];
-
-function filterArray(array, callback) {
-  const resultingArray = [];
-  // Write your filtering logic here
-  for (let item of array) {
-    const shouldBeIncluded = callback(item);
-    if (shouldBeIncluded) {
-      resultingArray.push(item);
-    }
-  }
-  return resultingArray;
-}
-
-/**
- * Challenge: Use your filter array method!
- * Given the above `people` array, return a new array with only people where `hasPet` is true
- * Note: Remember that your callback function will be given the individual item in the array for a parameter
- */
-
-const peopleWithPets = filterArray(people, function (person) {
-  return person.hasPet;
+document.getElementById("draw-card").addEventListener("click", function () {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("cards").innerHTML = `
+      <img src="${data.cards[0].image}">
+      <img src="${data.cards[1].image}">
+      `;
+    });
 });
 
-console.log(peopleWithPets);
+document.getElementById("new-deck").addEventListener("click", handleClick);
--- index.md
+++ index.md
@@ -4,3 +4,55 @@
     - Fulfilled - run .then() block
     - Rejected
 */
+
+
+// function callback() {
+//     console.log("I finally ran!")
+// }
+
+// setTimeout(callback, 2000)
+
+// const people = [
+//     { name: "Jack", hasPet: true },
+//     { name: "Jill", hasPet: false },
+//     { name: "Alice", hasPet: true },
+//     { name: "Bob", hasPet: false },
+// ]
+
+// function gimmeThePets(number) {
+//     return person.hasPet
+// }
+
+// const peopleWithPets = people.filter(gimmeThePets)
+// console.log(peopleWithPets)
+
+const people = [
+  { name: "Jack", hasPet: true },
+  { name: "Jill", hasPet: false },
+  { name: "Alice", hasPet: true },
+  { name: "Bob", hasPet: false },
+];
+
+function filterArray(array, callback) {
+  const resultingArray = [];
+  // Write your filtering logic here
+  for (let item of array) {
+    const shouldBeIncluded = callback(item);
+    if (shouldBeIncluded) {
+      resultingArray.push(item);
+    }
+  }
+  return resultingArray;
+}
+
+/**
+ * Challenge: Use your filter array method!
+ * Given the above `people` array, return a new array with only people where `hasPet` is true
+ * Note: Remember that your callback function will be given the individual item in the array for a parameter
+ */
+
+const peopleWithPets = filterArray(people, function (person) {
+  return person.hasPet;
+});
+
+console.log(peopleWithPets);
